46b057ecc771a840b9118eb5bac466608700745a
Clint Hakeman <clint.hakeman@gmail.com>
2012-05-15 13:36:30 -0500

    Starting C# WPF solution

---
+++ YAS4CG/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace YAS4CG
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
