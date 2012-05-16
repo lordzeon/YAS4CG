3a608f3bd95250fc39a3d64535943eed81a33fd3
Clint Hakeman <clint.hakeman@gmail.com>
2012-05-16 16:41:30 -0500

    - Added first rulebook (SR4A). - Added Matrix equipment ratings.

--- YAS4CG/Model/Resources.cs
+++ YAS4CG/Model/Resources.cs
@@ -9,6 +9,8 @@ namespace YAS4CG.Model
     {
         public string Name { get; set; }
 
+        public int Count { get; set; }
+
         public string Desc { get; set; }
     }
 
@@ -26,6 +28,58 @@ namespace YAS4CG.Model
         public Dictionary<string, string> Stats { get; set; }
 
         public int Cost { get; set; }
+    }
+
+    public sealed class DeviceRating
+    {
+        public static readonly DeviceRating DeviceRating1 = new DeviceRating(1, 250, 4, "Dumb. General appliances, bodyware.");
+        public static readonly DeviceRating DeviceRating2 = new DeviceRating(2, 750, 4, "Simple. Public terminals, entertainment systems.");
+        public static readonly DeviceRating DeviceRating3 = new DeviceRating(3, 1250, 8, "Average. Standard personal electronics, headware, vehicles, drones, home/business terminals.");
+        public static readonly DeviceRating DeviceRating4 = new DeviceRating(4, 2000, 8, "Complex. Security vehicles, alphaware, research terminals, security devices.");
+        public static readonly DeviceRating DeviceRating5 = new DeviceRating(5, 4000, 12, "Smart. High-end devices, betaware, security terminals, military vehicles.");
+        public static readonly DeviceRating DeviceRating6 = new DeviceRating(6, 8000, 16, "Cutting Edge. Deltaware, credsticks.");
+
+        public DeviceRating(int rating, int cost, int availability, string desc)
+        {
+            Rating = rating;
+            UpgradeCost = cost;
+            Availability = availability;
+            Desc = desc;
+        }
+
+        public int Rating { get; set; }
+
+        public int UpgradeCost { get; set; }
+
+        public int Availability { get; set; }
+
+        public string Desc { get; set; }
+    }
+
+    public sealed class SignalRating
+    {
+        public static readonly SignalRating SignalRating1 = new SignalRating(1, 10, 4, "40m. RFID tags, handheld electronics.");
+        public static readonly SignalRating SignalRating2 = new SignalRating(2, 50, 4, "100m. Headware transceivers, micro-drone sensors.");
+        public static readonly SignalRating SignalRating3 = new SignalRating(3, 150, 8, "400m. Average commlinks, residential/small business wi-fi routers, vehicular autonav sensors.");
+        public static readonly SignalRating SignalRating4 = new SignalRating(4, 500, 8, "1km. Crawler-drone sensors.");
+        public static readonly SignalRating SignalRating5 = new SignalRating(5, 1000, 12, "4km. Cyberlimb transceivers, heavy drone surveillance sensors.");
+        public static readonly SignalRating SignalRating6 = new SignalRating(6, 3000, 16, "10km. Cell-phone towers, public access wi-fi routers.");
+
+        public SignalRating(int rating, int cost, int availability, string desc)
+        {
+            Rating = rating;
+            UpgradeCost = cost;
+            Availability = availability;
+            Desc = desc;
+        }
+
+        public int Rating { get; set; }
+
+        public int UpgradeCost { get; set; }
+
+        public int Availability { get; set; }
+
+        public string Desc { get; set; }
     }
 
     internal class Augmentation : Resource
@@ -76,9 +130,11 @@ namespace YAS4CG.Model
 
     internal class Spell : Resource
     {
+        public string Type { get; set; }
+
         public Dictionary<string, string> Stats { get; set; }
 
-        public int Cost { get; set; }
+        public int Cost { get { return 3; } }
     }
 
     internal class Spirit : Resource
@@ -103,6 +159,7 @@ namespace YAS4CG.Model
 
         public int Cost { get; set; }
     }
+
     internal class Sprite : Resource
     {
         public Dictionary<string, Attribute> Attributes { get; set; }
